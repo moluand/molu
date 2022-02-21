e7e0222da1817e6b828cb78d1166ee029ce984a1
molu <2279126672@qq.com>
2022-02-21 09:15:43 +0800

    1

---
+++ src/main/java/molu.java
@@ -0,0 +1,10 @@
+/**
+ * @author molu
+ * @version 1.0
+ * @date 2022-02-21 09:13
+ */
+public class molu {
+    public static void main(String[] args) {
+        System.out.println("我是靓仔");
+    }
+}
